8135e313d74f3191749da9341198a6b7175a9c8c
ryota-sato07 <satour.0722@gmail.com>
2023-11-13 13:29:31 +0900

    create top page for quasar ui

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { Quasar } from 'quasar'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 
+import './style.css';
 import App from './App.vue'
 
 const myApp = createApp(App)
 
 myApp.use(Quasar, {
-  plugins: {}, // import Quasar plugins and add here
+  plugins: {},
 })
 
 myApp.mount('#app')
