c39beca3fd0aa1d118149714fc0c76dd121bb4f3
ryota-sato07 <satour.0722@gmail.com>
2023-11-13 19:06:55 +0900

    add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup lang="ts">
 import AppHeader from './components/common/AppHeader.vue'
 import AppFooter from './components/common/AppFooter.vue'
-import PageTop from './pages/Top.vue'
 </script>
 
 <template>
-  <div>
-    <q-layout view="lHh lpr lFf" container style="height: 100vh" class="shadow-2 rounded-borders">
-      <AppHeader/>
-      <AppFooter/>
-      <PageTop/>
-    </q-layout>
-  </div>
+  <q-layout view="lHh lpr lFf" container style="height: 100vh" class="shadow-2 rounded-borders">
+    <AppHeader/>
+    <AppFooter/>
+    <RouterView /> <!-- 追加 -->
+  </q-layout>
 </template>
