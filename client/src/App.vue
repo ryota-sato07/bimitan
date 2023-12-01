0306590681321ad2aa6c72d5ef1a2ec68903c6f6
ryota-sato07 <satour.0722@gmail.com>
2023-12-01 17:48:12 +0900

    delete auth ui

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,28 +2,6 @@
 import AppHeader from './components/common/AppHeader.vue'
 import AppFooter from './components/common/AppFooter.vue'
 import { RouterView } from 'vue-router'
-
-import "@aws-amplify/ui-vue/styles.css";
-import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
-
-import { Authenticator, translations } from "@aws-amplify/ui-vue";
-import { I18n } from 'aws-amplify/utils';
-I18n.putVocabularies(translations);
-I18n.setLanguage("ja");
-I18n.putVocabularies({
-  ja: {
-    "Sign In": "ログイン",
-    "Create Account": "アカウント作成",
-    Username: "Eメールアドレス",
-    "Enter your Username": "Eメールアドレスを入力",
-    Password: "ログインパスワード",
-    "Enter your Password": "ログインパスワードを入力",
-    "Sign in": "ログイン",
-    "Forgot your password?": "パスワードを再設定する",
-  },
-});
-Amplify.configure(awsconfig);
 </script>
 
 <template>
