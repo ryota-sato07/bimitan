b551ed7d16ec6ae2da540983f11b0f14fd39d865
ryota-sato07 <satour.0722@gmail.com>
2023-12-01 17:34:24 +0900

    add auth cognito

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,43 @@
 <script setup lang="ts">
 import AppHeader from './components/common/AppHeader.vue'
 import AppFooter from './components/common/AppFooter.vue'
+import { RouterView } from 'vue-router'
+
+// import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+import { Authenticator, translations } from "@aws-amplify/ui-vue";
+import { I18n } from 'aws-amplify/utils';
+I18n.putVocabularies(translations);
+I18n.setLanguage("ja");
+I18n.putVocabularies({
+  ja: {
+    "Sign In": "ログイン",
+    "Create Account": "アカウント作成",
+    Username: "Eメールアドレス",
+    "Enter your Username": "Eメールアドレスを入力",
+    Password: "ログインパスワード",
+    "Enter your Password": "ログインパスワードを入力",
+    "Sign in": "ログイン",
+    "Forgot your password?": "パスワードを再設定する",
+  },
+});
+Amplify.configure(awsconfig);
 </script>
 
 <template>
-  <q-layout view="lHh lpr lFf" container style="height: 100vh" class="shadow-2 rounded-borders">
-    <AppHeader/>
-    <AppFooter/>
-    <RouterView />
-  </q-layout>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <q-layout view="lHh lpr lFf" container style="height: 100vh" class="shadow-2 rounded-borders">
+        <AppHeader/>
+        <AppFooter/>
+        <RouterView />
+
+        <h1>Hello {{ user.username }}!</h1>
+        <button @click="signOut">Sign Out</button>
+      </q-layout>
+    </template>
+  </authenticator>
 </template>
